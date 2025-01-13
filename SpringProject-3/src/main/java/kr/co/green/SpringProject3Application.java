624878daebdf115d62fba215ba9187393916162f
nirsa <tnfalsdn12@naver.com>
2025-01-13 14:51:15 +0900

    test

---
+++ SpringProject-3/src/main/java/kr/co/green/SpringProject3Application.java
@@ -0,0 +1,13 @@
+package kr.co.green;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject3Application.class, args);
+	}
+
+}
---
+++ SpringProject-3/src/test/java/kr/co/green/SpringProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.green;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
